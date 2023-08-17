f9d2ce3f9503b9eea1e37f19bc5081c8992db324
happex <110610727+okayhappex@users.noreply.github.com>
2023-08-17 20:23:34 +0200

    :lipstick: Make some UI improvements

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,24 +11,24 @@
   import Python from '../components/prog/Python.vue'
 </script>
 <template>
-  <header class="bg-[url('/banner.png')] bg-cover bg-center bg-no-repeat text-white text-center rounded-3xl space-y-8">
-    <div class="bg-slate-800/10 backdrop-blur-2xl rounded-3xl p-8 space-y-4 shadow-2xl">
-      <img src="https://cdn2.scratch.mit.edu/get_image/user/84212389_500x500.png" class="w-32 h-32 mx-auto rounded-full shadow-2xl shadow-black/40"/>
+  <header class="bg-[url('/banner.png')] bg-cover bg-center bg-no-repeat text-white rounded--3xl md:text-center">
+    <div class="backdrop-blur-3xl rounded--3xl p-8 space-y-4 shadow-2xl">
+      <img src="https://cdn2.scratch.mit.edu/get_image/user/84212389_500x500.png" class="w-32 h-32 rounded-full shadow-2xl shadow-black/40 md:mx-auto"/>
       <div>
         <h1 class="text-4xl font-bold">happex</h1>
         <span class="block text-sm">@okayhappex</span>
       </div>
-      <p class="text-white/25 text-xs font-medium">He / Him - Ui/Ux designer</p>
-      <div class="flex space-x-4 place-content-center">
-        <a href="https://github.com/okayhappex"><Github className="fill-white w-6 h-6" /></a>
-        <a href="https://reddit.com/u/ha2pex"><Reddit className="stroke-white fill-white w-6 h-6" /></a>
-        <a href="https://dribbble.com/happex"><Dribbble className="fill-white w-6 h-6" /></a>
-        <a href="https://scratch.mit.edu/users/happex"><Scratch className="fill-white w-6 h-6" /></a>
-        <a href="https://darflen.com/users/6fed9705839fa26ef4f010e9"><Darflen className="fill-white w-6 h-6" /></a>
+      <p class="text-white/50 text-xs font-medium">he/him - Ui/Ux designer</p>
+      <div class="flex space-x-2 md:place-content-center md:space-x-3">
+        <button class="bg-white/10 rounded-full p-[6px] duration-150 hover:bg-white/20"><a href="https://github.com/okayhappex"><Github className="fill-white w-5 h-5" /></a></button>
+        <button class="bg-white/10 rounded-full p-[6px] duration-150 hover:bg-white/20"><a href="https://reddit.com/u/ha2pex"><Reddit className="stroke-white fill-white w-5 h-5" /></a></button>
+        <button class="bg-white/10 rounded-full p-[6px] duration-150 hover:bg-white/20"><a href="https://dribbble.com/happex"><Dribbble className="fill-white w-5 h-5" /></a></button>
+        <button class="bg-white/10 rounded-full p-[6px] duration-150 hover:bg-white/20"><a href="https://scratch.mit.edu/users/happex"><Scratch className="fill-white w-5 h-5" /></a></button>
+        <button class="bg-white/10 rounded-full p-[6px] duration-150 hover:bg-white/20"><a href="https://darflen.com/users/6fed9705839fa26ef4f010e9"><Darflen className="fill-white w-5 h-5" /></a></button>
       </div>
     </div>
   </header>
-  <div class="space-y-8 md:px-24">
+  <div class="px-8 space-y-8 md:px-24">
     <div class="space-y-4">
       <h2 class="text-xl font-bold">About me</h2>
       <p>Hey! It's happex, a French guy in my teens. I've been programming for more than six years and I'm in the web development since March, 2021.</p>
@@ -39,7 +39,7 @@
       <img src="https://skillicons.dev/icons?i=html,css,js,nodejs,tailwind,electron,vite,nextjs,vue,react,vercel,cloudflare,replit,github,git,vscode&perline=5" class="md:hidden" />
     </div>
   </div>
-  <section class="space-y-8 md:px-24">
+  <section class="px-8 space-y-8 md:px-24">
     <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
       <div class="bg-sky-600/30 text-center rounded-2xl p-4 space-y-2">
         <Tailwind className="w-16 h-16 mx-auto fill-sky-600" />
@@ -63,4 +63,7 @@
       </div>
     </div>
   </section>
+  <footer class="text-center p-8 md:px-24">
+    <div>(c) happex 2023 - Present</div>
+  </footer>
 </template>
